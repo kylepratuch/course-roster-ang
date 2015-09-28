2180c5c7b9f7b91dbb95e15832314dc64a25b20b
Kyle Pratuch & Chris Lytsell <student@epicodus.com>
2015-09-28 10:26:18 -0700

    add multiple views (header)

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
 //Declaring angular module and passing in ui.router dependency:
 var courseRoster = angular.module('courseRoster', ['ui.router']);
 
-  //The UI's 'home', 'courses', and 'courses.students' states w/ URL and partials/template:
-  courseRoster.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('home', {
-      url:"",
-      templateUrl: "partials/home.html"
-    });
+//The UI's 'home', 'courses', and 'courses.students' states w/ URL and partials/template:
+courseRoster.config(function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider.state('courses', {
-      url:"/courses",
-      templateUrl:"partials/courses.html",
-      controller:'CoursesCtrl'
-    });
+  $stateProvider.state('home', {
+    url:"",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+        controller: 'HeadersCtrl',
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
+  });
 
-    //This is a nested state; 'students' is nested in 'courses':
-    $stateProvider.state('courses.students', {
-      url: "/:courseId",
-      templateUrl: "partials/courses.students.html",
-      controller: 'StudentsCtrl'
-    });
+  $stateProvider.state('courses', {
+    url:"/courses",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+        controller: 'HeadersCtrl',
+      },
+      'body': {
+        templateUrl: "partials/courses.html",
+        controller: 'CoursesCtrl'
+      },
+    }
+  });
+
+  //This is a nested state; 'students' is nested in 'courses':
+  $stateProvider.state('courses.students', {
+    url: "/:courseId",
+    views: {
+      'header' : {
+        templateUrl: 'partials/header.html',
+        controller: 'HeadersCtrl',
+      },
+      'body': {
+        templateUrl: "partials/courses.students.html",
+        controller: 'StudentsCtrl'
+      },
+    }
+  });
 });
